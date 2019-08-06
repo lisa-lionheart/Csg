6e76b009adb36ddaf6b8973fbd24fb25819f2e11
Lisa Croxford <lisa@croxford.me>
2019-08-06 09:32:09 +0100

    2DBitArray.cs

---
+++ Csg/2DBitArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csg
+{
+    class BitArray2D {
+        readonly int Width;
+        readonly int Height;
+
+        readonly private BitArray array;
+
+
+        public BitArray2D(int width, int height) {
+            this.Width = width;
+            this.Height = height;
+            array = new BitArray(width * height);
+        }
+
+        public bool this[int x, int y] {
+            get {
+                return array[(x * Width) + y];
+			}
+            set {
+                array[(x * Width) + y] = value;
+            }
+        }
+    }
+}
